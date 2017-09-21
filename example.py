0b55b09883de898b3d7df00b8116f34ddeb5ddf8
Kuznetsov Denis <kuznetsov.den.p@gmail.com>
2017-09-21 21:45:49 +0300

    Added environ var CHITCHAT_CKPT

--- example.py
+++ example.py
@@ -3,7 +3,8 @@ import os
 
 
 model_path = os.environ.get("CHITCHAT_MODEL")
-bot.init(model_path)
+ckpt_name = os.environ.get("CHITCHAT_CKPT") #Optional
+bot.init(model_path, ckpt_name) # if ckpt_name is None bot gets last ckpt
 
 #print(bot.send('Здравствуйте , Подскажите , веду бух учёт на Эльбе . Контур . Сохранил платёжные поручения для уплаты страховых платежей , сохранил их в текстовом формате  ( так сохраняет сама программа ), а в банк не могу импортировать . пишет , что в файле нет необходимых реквизитов , хотя года открываешь текстовый файл вся информация присутствует . Спасибо .'))
 
