5af077dc15f6b891d6aba3979f24c44f8d25abb9
Kuznetsov Denis <kuznetsov.den.p@gmail.com>
2017-11-02 15:03:19 +0300

    Added script with bpe.

---
+++ start_bpe_train.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+export model_path="/home/kuznetsov/tmp/thread21"
+export generator_path="/home/kuznetsov/embeddings/fasttext/models/model-l+w+2t-1/lenta+wiki+ted+tedx-1.bin"
+rm -rf ${model_path}/model
+mkdir  ${model_path}/model
+#--pretrain_enc_emb_path=${model_path}/data/enc_embeddings.emb \
+#--pretrain_dec_emb_path=${model_path}/data/dec_embeddings.emb \
+#--share_vocab True \
+# --num_gpus 2 \
+# --embedding_generator_path=${generator_path} \
+# --pretrain_enc_emb_path=${model_path}/data/enc_embeddings.emb \
+# --pretrain_dec_emb_path=${model_path}/data/dec_embeddings.emb \
+#
+
+
+
+python -m nmt.nmt     \
+  --src=cor --tgt=man     \
+  --vocab_prefix=${model_path}/data/vocab     \
+  --train_prefix=${model_path}/data/train     \
+  --dev_prefix=${model_path}/data/dev_test     \
+  --test_prefix=${model_path}/data/test     \
+  --out_dir=${model_path}/model     \
+  --bpe_delimiter @@ \
+  --num_gpus 2 \
+  --share_vocab True \
+  --encoder_type bi \
+  --batch_size 128     \
+  --num_train_steps=3000000     \
+  --steps_per_stats=100     \
+  --num_layers=2     \
+  --num_units=1024     \
+  --dropout=0.2     \
+  --src_max_len=140    \
+  --tgt_max_len=140  \
+  --attention=luong     \
+  --metrics=bleu
--- start_debug.sh
+++ start_debug.sh
@@ -4,9 +4,11 @@ export model_path="/home/kuznetsov/tmp/thread17"
 export generator_path="/home/kuznetsov/embeddings/fasttext/models/model-l+w+2t-1/lenta+wiki+ted+tedx-1.bin"
 rm -rf ${model_path}/model
 mkdir  ${model_path}/model
-#--pretrain_dec_emb_path=${model_path}/data/enc_embeddings.emb \
+#--pretrain_enc_emb_path=${model_path}/data/enc_embeddings.emb \
 #--pretrain_dec_emb_path=${model_path}/data/dec_embeddings.emb \
-
+#--share_vocab True \
+# --num_gpus 2 \
+#
 
 
 
@@ -14,13 +16,13 @@ python -m nmt.nmt     \
   --src=cor --tgt=man     \
   --embedding_generator_path=${generator_path} \
   --pretrain_enc_emb_path=${model_path}/data/enc_embeddings.emb \
-  --pretrain_dec_emb_path=${model_path}/data/enc_embeddings.emb \
-  --share_vocab True \
+  --pretrain_dec_emb_path=${model_path}/data/dec_embeddings.emb \
   --vocab_prefix=${model_path}/data/vocab     \
   --train_prefix=${model_path}/data/train     \
   --dev_prefix=${model_path}/data/dev_test     \
   --test_prefix=${model_path}/data/test     \
   --out_dir=${model_path}/model     \
+  --num_gpus 2 \
   --encoder_type bi \
   --batch_size 128     \
   --num_train_steps=3000000     \
